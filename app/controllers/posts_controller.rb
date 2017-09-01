60cd7ae23326a4f7cd1ec60311e0d11404c2a530
NadimRai <rai_nadim@yahoo.com>
2017-09-02 01:21:39 +0800

    only owner can edit and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,18 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	before_action :owned_post, only: [:edit, :update, :destroy]
 
 	def index
 		@posts = Post.all 
 	end
 
 	def new
-		@post = Post.new
+		@post = current_user.posts.build
 	end
 
 	def create
-		@post = Post.new(post_params)
+		@post = current_user.posts.build(post_params)
 		if @post.save
 			flash[:success] = "Your post has been created!"
 			redirect_to root_path 
@@ -53,4 +54,11 @@ class PostsController < ApplicationController
 	def post_params
 		params.require(:post).permit(:caption, :image)
 	end
+
+	def owned_post
+	  unless current_user == @post.user
+	    flash[:alert] = "That post doesn't belong to you!"
+	    redirect_to root_path
+	  end
+  	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
 	belongs_to :user
+	
 	validates :image, presence: true
+	validates :user_id, presence: true
 
 	has_attached_file :image, styles: { :medium => "640x" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
