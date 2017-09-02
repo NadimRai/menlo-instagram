8243ddbbf43a636b3039f3ba85ceb9b934ae22c2
NadimRai <rai_nadim@yahoo.com>
2017-09-03 01:55:44 +0800

    generated profiles controller and ability to display user only post in profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do 
@@ -6,5 +8,5 @@ Rails.application.routes.draw do
   end
 
   root 'posts#index'
-  get ':user_name', to: 'profile#show', as: :profile
+  get ':user_name', to: 'profiles#show', as: :profile
 end
